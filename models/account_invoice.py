2fa3598b6c904a5ed64d70d23eb0781bed026a8e
EDallas <inmsanz89@gmail.com>
2020-10-16 12:39:49 +0100

    IMP purchase.order funcionality
    Add purchase order onchange
    Add internal_note from purchase.order to invoice default values

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -8,8 +8,16 @@ class AccountInvoice(models.Model):
 
     @api.onchange('internal_note')
     def _onchange_internal_note(self):
+        # sale order
         so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1)
         so.write({'internal_note' : self.internal_note})
 
-        inv = self.env['account.invoice'].search([('origin', '=', self.origin)])
-        inv.write({'internal_note' : self.internal_note})
+        so_inv = self.env['account.invoice'].search([('origin', '=', self.origin)])
+        so_inv.write({'internal_note' : self.internal_note})
+
+        #purchase order
+        po =  self.env['purchase.order'].search([('name', '=', self.origin)], limit=1)
+        po.write({'internal_note' : self.internal_note})
+
+        po_inv = self.env['account.invoice'].search([('origin', '=', self.origin)])
+        po_inv.write({'internal_note' : self.internal_note})
--- models/purchase_order.py
+++ models/purchase_order.py
@@ -4,3 +4,13 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     internal_note = fields.Text(string="Internal Note")
+
+    @api.onchange('internal_note')
+    def _onchange_internal_note(self):
+        po = self.env['account.invoice'].search([('origin', '=', self.name)])
+        po.write({'internal_note' : self.internal_note})
+
+    def action_view_invoice(self):
+        result = super(PurchaseOrder, self). action_view_invoice()
+        result["context"]['default_internal_note'] = self.internal_note
+        return result
