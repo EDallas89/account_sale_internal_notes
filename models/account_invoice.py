3a83ea6b949c0fef18e425ef4f671dbf766ab24c
EDallas <inmsanz89@gmail.com>
2020-10-19 12:47:34 +0100

    IMP Multiple origin on SaleOrder and AccountInvoice

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -8,16 +8,19 @@ class AccountInvoice(models.Model):
 
     @api.onchange('internal_note')
     def _onchange_internal_note(self):
-        # sale order
-        so = self.env['sale.order'].search([('name', '=', self.origin)], limit=1)
-        so.write({'internal_note' : self.internal_note})
+    # Change internal_note field on all its origins and any other invoice with the same origin
+        origin_split = self.origin.split(", ")
+        for origin in origin_split:
+            # sale order
+            so = self.env['sale.order'].search([('name', '=', origin)], limit=1)
+            so.write({'internal_note' : self.internal_note})
 
-        so_inv = self.env['account.invoice'].search([('origin', '=', self.origin)])
-        so_inv.write({'internal_note' : self.internal_note})
+            so_inv = self.env['account.invoice'].search([('origin', '=', origin)])
+            so_inv.write({'internal_note' : self.internal_note})
 
-        #purchase order
-        po =  self.env['purchase.order'].search([('name', '=', self.origin)], limit=1)
-        po.write({'internal_note' : self.internal_note})
+            #purchase order
+            po =  self.env['purchase.order'].search([('name', '=', origin)], limit=1)
+            po.write({'internal_note' : self.internal_note})
 
-        po_inv = self.env['account.invoice'].search([('origin', '=', self.origin)])
-        po_inv.write({'internal_note' : self.internal_note})
+            po_inv = self.env['account.invoice'].search([('origin', '=', origin)])
+            po_inv.write({'internal_note' : self.internal_note})
--- models/sale_order.py
+++ models/sale_order.py
@@ -13,5 +13,14 @@ class SaleOrder(models.Model):
     
     @api.onchange('internal_note')
     def _onchange_internal_note(self):
-        so = self.env['account.invoice'].search([('origin', '=', self.name)])
-        so.write({'internal_note' : self.internal_note})
+    # Change internal_note field on all its invoices
+        invoices = self.env['account.invoice'].search([('origin', 'like', self.name)])
+        for inv in invoices:
+            inv.write({'internal_note' : self.internal_note})
+    # Change internal_note field on any other origins from its invoices
+            origin_split = inv.origin.split(", ")
+            origin_len = len(origin_split)
+            if (origin_len > 1):
+                for origin in origin_split:
+                    so = self.env['sale.order'].search([('name', '=', origin)])
+                    so.write({'internal_note' : self.internal_note})
