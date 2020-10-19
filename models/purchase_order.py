8106593cd8fb241ef47a2c52edddaf962aa1617d
EDallas <inmsanz89@gmail.com>
2020-10-19 13:28:05 +0100

    IMP Multiple origin on PurchaseOrder

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -7,8 +7,20 @@ class PurchaseOrder(models.Model):
 
     @api.onchange('internal_note')
     def _onchange_internal_note(self):
-        po = self.env['account.invoice'].search([('origin', '=', self.name)])
-        po.write({'internal_note' : self.internal_note})
+#        po = self.env['account.invoice'].search([('origin', '=', self.name)])
+#        po.write({'internal_note' : self.internal_note})
+
+    # Change internal_note field on all its invoices
+        invoices = self.env['account.invoice'].search([('origin', 'like', self.name)])
+        for inv in invoices:
+            inv.write({'internal_note' : self.internal_note})
+    # Change internal_note field on any other origins from its invoices
+            origin_split = inv.origin.split(", ")
+            origin_len = len(origin_split)
+            if (origin_len > 1):
+                for origin in origin_split:
+                    po = self.env['purchase.order'].search([('name', '=', origin)])
+                    po.write({'internal_note' : self.internal_note})
 
     def action_view_invoice(self):
         result = super(PurchaseOrder, self). action_view_invoice()
