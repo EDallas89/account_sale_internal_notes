af968156823f005447e7552ad05c1c2c1c31e391
EDallas <inmsanz89@gmail.com>
2020-10-21 14:36:21 +0100

    IMP funcionality on credit notes

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -12,16 +12,33 @@ class AccountInvoice(models.Model):
         if self.origin:
             origin_split = self.origin.split(", ")
             for origin in origin_split:
-                # sale order
+                ## sale order
+                # search quotations
                 so = self.env['sale.order'].search([('name', '=', origin)], limit=1)
                 so.write({'internal_note' : self.internal_note})
-    
+                # search others invoices
                 so_inv = self.env['account.invoice'].search([('origin', '=', origin)])
                 so_inv.write({'internal_note' : self.internal_note})
     
-                #purchase order
+                ## purchase order
+                # search purchase orders
                 po =  self.env['purchase.order'].search([('name', '=', origin)], limit=1)
                 po.write({'internal_note' : self.internal_note})
-    
+                # search others invoices
                 po_inv = self.env['account.invoice'].search([('origin', '=', origin)])
                 po_inv.write({'internal_note' : self.internal_note})
+
+                ## credit note
+                # search credit notes
+                cn = self.env['account.invoice'].search([('origin', '=', self.number)])
+                cn.write({'internal_note' : self.internal_note})
+
+        # search invoices
+        cn = self.env['account.invoice'].search([('number', '=', self.origin)])
+        cn.write({'internal_note' : self.internal_note})
+
+    @api.model
+    def refund(self, date_invoice=None, date=None, description=None, journal_id=None):
+        values = super(AccountInvoice, self).refund()
+        values['internal_note'] = self.internal_note
+        return values
